02e8ebfc5bc36ac89a5bdf7679d5f1d039bc273f
fede <fede.lopez@gmail.com>
2020-01-29 20:49:32 +1100

    Return empty rows when no data

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ const pool = new Pool({
 
 app.get('/movies/:offset', async (req, res) => {
     const client = await pool.connect();
-    const count = await client.query('SELECT COUNT(1) as total from MOVIES');
-    const result = await client.query(`SELECT movie_title, movie_imdb_link, imdb_score FROM MOVIES order by imdb_score desc LIMIT 25 OFFSET ${req.params.offset}`);
-    res.send({count: count.rows[0].total, rows: result.rows});
+    try {
+        const count = await client.query('SELECT COUNT(1) as total from MOVIES');
+        const result = await client.query(`SELECT movie_title, movie_imdb_link, imdb_score FROM MOVIES order by imdb_score desc LIMIT 25 OFFSET ${req.params.offset}`);
+        res.send({count: count.rows[0].total, rows: result.rows});
+    } catch (error) {
+        console.error('Could not retrieve movies from db', error);
+        res.send({count: 0, rows: []});
+    }
     client.release();
 });
 
